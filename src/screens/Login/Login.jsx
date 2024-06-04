ccaf0e6e57f7edea04094186041b728bcbcfda8c
Lreategui-24 <162217611+Lreategui-24@users.noreply.github.com>
2024-06-04 13:08:22 -0300

    4/6/24

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -56,6 +56,7 @@ export const Login = ({state}) => {
         <div className="overlap-group-25">
           <img className="portal" alt="Portal" src="/img/portal.png" />
           <div className="rectngulo-49" />
+          <div className="rectangulo-001">
           <div className="rectngulo-50" />
           <div className="text-wrapper-272">¡Hola!</div>
           <p className="ingresa-tu-correo">
@@ -64,27 +65,29 @@ export const Login = ({state}) => {
             </span>
             <span className="text-wrapper-274">PCP</span>
           </p>
-          <div className="text-wrapper-275">Correo electrónico</div>
-          <div className="rectngulo-51" />
-          <div className="componente-26">
-            <img className="trazado-72" alt="Trazado" src="/img/trazado-2500.png" />
+          
+            <div className="text-wrapper-275">Correo electrónico</div>
+            <div className="rectngulo-51" />
+            <div className="componente-26">
+              <img className="trazado-72" alt="Trazado" src="/img/trazado-2500.png" />
+            </div>
+            <div className="rectngulo-52" /> 
+            <form className="form" onSubmit={handleSubmit} >
+              <input type="email" placeholder="Correo Electronico" className="correo-electronico" value={email} onChange={e => setEmail(e.target.value)}/>
+              <input type="password"placeholder="Contraseña" className="contrasea" value={password} onChange={e => setPassword(e.target.value)}/>
+              <button onClick={handlebutton} className="text-wrapper-276">Iniciar sesion</button>
+              <div className="text-wrapper-277">Recuperar contraseña?</div>
+              <div className="text-wrapper-278">Contraseñas</div>
+            </form>
+            {error && <p>obligatorios</p>}
+            <p className="text-wrapper-279">Términos y Condiciones de uso</p>
+            <img className="chevron-right-19" alt="Chevron right" src="/img/chevron-right-20.png" />
+            <img
+              className="logo-placeholder-6"
+              alt="Logo placeholder"
+              src="/img/logo-placeholder-3-removebg-preview-5.png"
+            />
           </div>
-          <div className="rectngulo-52" /> 
-          <form onSubmit={handleSubmit} >
-            <input type="email" placeholder="Correo Electronico" className="correo-electronico" value={email} onChange={e => setEmail(e.target.value)}/>
-            <input type="password"placeholder="Contraseña" className="contrasea" value={password} onChange={e => setPassword(e.target.value)}/>
-            <button onClick={handlebutton} className="text-wrapper-276">Iniciar sesion</button>
-            <div className="text-wrapper-277">Recuperar contraseña?</div>
-            <div className="text-wrapper-278">Contraseñas</div>
-          </form>
-          {error && <p>obligatorios</p>}
-          <p className="text-wrapper-279">Términos y Condiciones de uso</p>
-          <img className="chevron-right-19" alt="Chevron right" src="/img/chevron-right-20.png" />
-          <img
-            className="logo-placeholder-6"
-            alt="Logo placeholder"
-            src="/img/logo-placeholder-3-removebg-preview-5.png"
-          />
         </div>
       </div>
     </div>
