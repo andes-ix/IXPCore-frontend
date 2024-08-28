9524944c7448630d33cd236e1e037181872bd9c6
Jovan Pacheco <jovanjpacheco@gmail.com>
2024-08-28 18:01:35 -0500

    Otra prueba

--- src/pages/Dashboards/Ecommerce/WelcomeWidget.tsx
+++ src/pages/Dashboards/Ecommerce/WelcomeWidget.tsx
@@ -792,7 +792,7 @@ const WelcomeWidget = () => {
                 type="button"
                 className="text-white btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-500/20 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-500/20 dark:ring-custom-400/20"
               >
-                Take a Product
+                Compra un producto
               </button>
             </div>
             <div className="hidden col-span-12 2xl:col-span-3 lg:col-span-2 lg:col-start-11 2xl:col-start-10 lg:block">
