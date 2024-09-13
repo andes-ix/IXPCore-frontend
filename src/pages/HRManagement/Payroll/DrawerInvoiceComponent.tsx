e4dcc8623e62cc2e897553604a908b59ba904cee
MonasterioY <jose.monasterio@lyncros.com>
2024-09-13 17:44:55 -0400

    feat: add new invocie viewer

--- src/pages/HRManagement/Payroll/DrawerInvoiceComponent.tsx
+++ src/pages/HRManagement/Payroll/DrawerInvoiceComponent.tsx
@@ -9,6 +9,7 @@ import {
   BLUE100,
   BLUE250,
   BLUE50,
+  GREY1,
   GREY10,
   GREY100,
   GREY15,
@@ -16,6 +17,7 @@ import {
 } from "Common/constants/colors";
 import { Download, QrCode, X } from "lucide-react";
 import moment from "moment";
+import { CustomTableSimpleComponent } from "Common/Components/CustomTableSimple/customTableSimple";
 
 interface DrawerInvoiceProps {
   handleDrawerOpen: () => void;
@@ -29,27 +31,15 @@ const DrawerInvoiceComponent: React.FC<DrawerInvoiceProps> = ({
   itemDetail,
 }) => {
   const { extra_details } = itemDetail;
-  // const columnsTest = [
-  //   { key: "id", label: "Order ID" },
-  //   { key: "shop", label: "Shop" },
-  //   { key: "customer", label: "Customer" },
-  //   { key: "price", label: "Price" },
-  // ];
+  const columnsTest = [
+    { key: "id", label: "Descripción" },
+    { key: "shop", label: "Cantidad" },
+    { key: "customer", label: "Precio unitario" },
+    { key: "price", label: "Impuestos" },
+    { key: "price", label: "Importe" },
+  ];
 
-  // const data = [
-  //   {
-  //     id: "#541254265",
-  //     shop: "Amezon",
-  //     customer: "Cleo Carson",
-  //     price: "$4,521",
-  //   },
-  //   {
-  //     id: "#744145235",
-  //     shop: "Shoppers",
-  //     customer: "Juston Eichmann",
-  //     price: "$7,546",
-  //   },
-  // ];
+  const data: any = [];
   return (
     <CustomDrawerComponent
       handleCustomDrawer={handleDrawerOpen}
@@ -92,165 +82,125 @@ const DrawerInvoiceComponent: React.FC<DrawerInvoiceProps> = ({
 
           <div className="flex pl-10 pr-10 justify-between ">
             {/* section left to drawer */}
-            <div className=" w-1/2 p-10 mb-10 border-solid border-2 border-[#EFEFEF] rounded-md">
-              <div className=" flex pb-10 justify-between ">
-                <div>
-                  <img className="w-16 h-22" src={"/img/logo.webp"} alt="PIT" />
-                </div>
-                {itemDetail?.extra_details?.file && (
-                  <span
-                    className="cursor-pointer"
-                    onClick={() => {
-                      const fileUrl = itemDetail?.extra_details?.file;
-                      if (fileUrl) {
-                        const link = document.createElement("a");
-                        link.href = fileUrl;
-                        link.setAttribute("download", "invoice.pdf"); // El nombre del archivo descargado será "invoice.pdf"
-                        link.setAttribute("target", "_blank"); // Abrir en nueva ventana para asegurar que no lo reemplace
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link); // Eliminar el enlace temporal del DOM
-                      }
-                    }}
+            <div className=" w-3/4 p-10 mb-10 border-solid border-2 border-[#EFEFEF] rounded-md">
+              <div className=" flex pb-10 justify-between  ">
+                <div className="card shadow-none w-1/3 h-72 rounded-md">
+                  <div
+                    className={`h-1/3 bg-[${GREY1}]  flex justify-between  `}
                   >
-                    <div
-                      className={`flex items-center justify-center size-10 rounded-md  bg-[${BLUE10}]`}
-                    >
-                      <Download className="size-5" color={"white"} />
+                    <div className="flex flex-col pl-5 leading-5 justify-center">
+                      <Text
+                        bold="bold"
+                        size={"big-sm"}
+                        color={GREY150}
+                        text={"Factura"}
+                      />
+                      <Text
+                        color={BLUE10}
+                        bold="bold"
+                        size="big"
+                        text={itemDetail?.number}
+                      />
                     </div>
-                  </span>
-                )}
-              </div>
-              <div>
-                <Text
-                  size={"big-sm"}
-                  color={GREY150}
-                  text={extra_details?.isp_name}
-                ></Text>
-                <Text
-                  size={"big"}
-                  color={BLUE100}
-                  text={"RUC 20605039546"}
-                ></Text>
-                <Text
-                  color={GREY150}
-                  size={"medium"}
-                  text={extra_details?.pit_direction_1}
-                ></Text>
-                <Text
-                  color={GREY150}
-                  bold={"bold"}
-                  size={"big-sm"}
-                  text={extra_details?.pit_direction_2?.toUpperCase()}
-                ></Text>
-              </div>
-              <div className="flex pt-5 pb-5 justify-evenly">
-                <div className="flex flex-col items-center">
-                  <Text
-                    color={BLUE10}
-                    bold={"semi-bold"}
-                    size={"big-sm"}
-                    text={moment(itemDetail.invoice_date_due).format(
-                      "DD/MM/YYYY"
-                    )}
-                  />
-                  <Text color={GREY15} size={"big-sm"} text={"Vencimiento"} />
-                </div>
-                <div className="flex flex-col items-center">
-                  <span className="px-2.5 py-0.5 inline-block text-xs font-medium rounded border bg-green-100 border-transparent text-green-500 dark:bg-green-500/20 dark:border-transparent">
-                    <div className="flex gap-1 justify-center items-center">
-                      {itemDetail?.payment_state}
+                    <div className="pt-4 pr-4">
+                      <span className="px-2.5 py-0.5 inline-block text-xs font-medium rounded border bg-green-100 border-transparent text-green-500 dark:bg-green-500/20 dark:border-transparent">
+                        <div className="flex gap-1 justify-center items-center">
+                          {itemDetail?.payment_state}
+                        </div>
+                      </span>
                     </div>
-                  </span>
-                  <Text color={GREY15} size={"big-sm"} text={" Estatus"} />
-                </div>
-                <div className="flex flex-col items-center">
-                  <Text
-                    color={BLUE10}
-                    bold={"semi-bold"}
-                    size={"big-sm"}
-                    text={`$ ${itemDetail?.amount_total}`}
-                  />
-                  <Text color={GREY15} size={"big-sm"} text={"Total"} />
-                </div>
-              </div>
-              <div>
-                <Text
-                  color={GREY150}
-                  bold={"bold"}
-                  size={"big-sm"}
-                  text={"Datos del cliente"}
-                ></Text>
-                <Text
-                  color={BLUE50}
-                  size={"medium"}
-                  text={itemDetail?.extra_details?.isp_name}
-                ></Text>
-                <Text
-                  size={"medium"}
-                  color={GREY15}
-                  text={extra_details?.pit_document}
-                ></Text>
-                <Text
-                  size={"medium"}
-                  color={GREY100}
-                  bold={"semi-bold"}
-                  text={"Dirección"}
-                ></Text>
-                <Text
-                  color={GREY15}
-                  size={"medium"}
-                  text={itemDetail?.extra_details?.isp_direction_text}
-                ></Text>
-              </div>
-              <div className="flex pt-10 justify-between ">
-                <div className="w-1/3">
-                  <Text
-                    color={GREY150}
-                    size={"big-sm"}
-                    text="Detraccion"
-                    bold={"bold"}
-                  ></Text>
-                  <Text
-                    color={GREY15}
-                    text={extra_details?.detraccion_comentary}
-                    size={"medium"}
-                  />
-                </div>
-                <div className="w-1/3">
-                  <Text
-                    color={GREY150}
-                    size={"big-sm"}
-                    text="Descripcion"
-                    bold={"bold"}
-                  ></Text>
-                  <Text
-                    color={GREY15}
-                    text={extra_details?.detraccion_description}
-                    size={"medium"}
-                  />
+                  </div>
+
+                  <div className={`h-1/3 bg-[#F9F9F9]  flex justify-between  `}>
+                    <div className="flex flex-col pl-5 leading-5 justify-center">
+                      <Text
+                        bold="bold"
+                        size={"big-sm"}
+                        color={GREY150}
+                        text={"Vencimiento"}
+                      />
+                      <Text
+                        color={BLUE10}
+                        bold={"semi-bold"}
+                        size={"big-sm"}
+                        text={moment(itemDetail.invoice_date_due).format(
+                          "DD/MM/YYYY"
+                        )}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`h-1/3 bg-[${GREY1}]  flex justify-between  `}
+                  >
+                    <div className="flex flex-col pl-5 leading-5 justify-center">
+                      <Text
+                        bold="bold"
+                        size={"big-sm"}
+                        color={GREY150}
+                        text={"Total"}
+                      />
+                      <Text
+                        color={BLUE10}
+                        bold={"semi-bold"}
+                        size={"big-sm"}
+                        text={`$ ${itemDetail?.amount_total || 0} `}
+                      />
+                    </div>
+                  </div>
                 </div>
 
-                <div className="w-1/4">
-                  <Title
-                    color={BLUE10}
-                    bold={"bold"}
-                    size={"normal-xl"}
-                    text={extra_details?.detraccion_amount}
-                  ></Title>
-                  <Text
-                    color={GREY10}
-                    bold={"bold"}
-                    text={"Monto detraccion"}
-                    size={"medium"}
-                  />
+                <div className=" pl-4 w-3/4  h-72 ">
+                  <div
+                    className={`flex flex-col p-5 h-full rounded-md  bg-[#F9F9F9]`}
+                  >
+                    <Text
+                      size={"big"}
+                      className="pb-5"
+                      bold="bold"
+                      color={GREY150}
+                      text={"Cargos adicionales"}
+                    />
+                    <div className="border-solid border-t-2 border-[#EFEFEF]"></div>
+                    <div className="flex justify-between pt-5">
+                      <div>
+                        <Text
+                          color={GREY150}
+                          size={"big-sm"}
+                          bold="bold"
+                          text="Impuestos y detracciones"
+                        />
+                        <Text
+                          size={"medium"}
+                          color={GREY15}
+                          text={
+                            "Operación sujeta al SPOT con el gobierno central..."
+                          }
+                        />
+                      </div>
+                      <div>
+                        <Title
+                          size={"medium"}
+                          color={BLUE10}
+                          bold="bold"
+                          text={extra_details?.amount_tax}
+                        />
+                        <Text
+                          size={"big-sm"}
+                          text={"Monto total"}
+                          color={GREY10}
+                          bold={"bold"}
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
+
               <div className="pt-5">
-                {/* <CustomTableSimpleComponent
+                <CustomTableSimpleComponent
                   columns={columnsTest}
                   data={data}
-                ></CustomTableSimpleComponent> */}
+                ></CustomTableSimpleComponent>
                 <div className="flex pt-5 justify-end">
                   <div className="flex w-1/2">
                     <table className="w-full">
@@ -302,121 +252,16 @@ const DrawerInvoiceComponent: React.FC<DrawerInvoiceProps> = ({
               </div>
             </div>
             {/* section rigth to drawer */}
-            <div className=" w-1/2 pl-10 pt-6 mb-10 ">
-              <div className="w-2/3">
-                <div>
-                  <Text
-                    color={GREY150}
-                    bold={"bold"}
-                    size={"big-sm"}
-                    text={"Observaciones"}
-                  ></Text>
-
-                  <Text
-                    size={"medium"}
-                    className="pt-3"
-                    color={GREY15}
-                    text={
-                      "Por favor utilice la siguiente referencia al realizar"
-                    }
-                  ></Text>
-                  <div className="flex gap-1">
-                    <Text
-                      size={"medium"}
-                      color={GREY15}
-                      text={"su pago:"}
-                    ></Text>
-                    <Text
-                      color={BLUE50}
-                      bold="bold"
-                      size={"medium"}
-                      text={itemDetail?.number}
-                    ></Text>
-                  </div>
-                  <Text
-                    className="pt-3"
-                    size={"medium"}
-                    color={GREY15}
-                    text={
-                      "Autorizado mediante resolución de Superintendencia Inc c Art 2 RS 155-2017/SUNAT. BANCO DE LA NACION - CTA CTE: 00-074-173713."
-                    }
-                  ></Text>
-                </div>
-
-                <div className="pt-5">
-                  <div className="flex pt-5">
-                    <table className="w-full">
-                      <thead className="ltr:text-left rtl:text-right ">
-                        <tr className="">
-                          <th className="px-3.5 py-2.5 font-semibold text-[#8A8F9C]  bg-[#F1F5F9]">
-                            Base imponible
-                          </th>
-                          <th className="px-3.5 py-2.5 font-semibold text-[#8A8F9C]  bg-[#F1F5F9]">
-                            $ {extra_details?.amount_untaxed || 0}
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr className="odd:bg-white even:bg-slate-50 dark:odd:bg-zink-600 dark:even:bg-zink-700">
-                          <td className="px-3.5 py-2.5 border-y border-slate-200 dark:border-zink-500">
-                            <Text text="IGV" color={GREY10} size="medium-sm" />
-                          </td>
-                          <td className="px-3.5 py-2.5 border-y border-slate-200 dark:border-zink-500">
-                            <Text
-                              text={`$ ${extra_details?.amount_tax || 0}`}
-                              color={BLUE250}
-                              size="medium"
-                            />
-                          </td>
-                        </tr>
-                        <tr className="odd:bg-white even:bg-slate-50 dark:odd:bg-zink-600 dark:even:bg-zink-700">
-                          <td className="px-3.5 py-2.5 border-y border-slate-200 dark:border-zink-500">
-                            <Text
-                              text="Total"
-                              color={GREY10}
-                              size="medium-sm"
-                              bold="bold"
-                            />
-                          </td>
-                          <td className="px-3.5 py-2.5 border-y border-slate-200 dark:border-zink-500">
-                            <Text
-                              text={`$ ${itemDetail?.amount_total || 0}`}
-                              color={GREY10}
-                              size="medium"
-                              bold="bold"
-                            />
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-
-                <div className="pt-5">
-                  <Text
-                    color={BLACK200}
-                    bold={"bold"}
-                    size={"big"}
-                    text={"Son"}
-                  ></Text>
-
-                  <Text
-                    color={GREY15}
-                    size={"medium"}
-                    className="pt-3"
-                    text={
-                      "Operación sujeta al sistema de Pago de Obligaciones Tributarias-SPOT, Banco de la Nacion 12.0% Cod Serv. 037"
-                    }
-                  ></Text>
-
-                  <QrCode className="size-20 pl-4 pt-5" />
-                  <Text
-                    color={GREY15}
-                    size={"medium-sm"}
-                    className="pt-3"
-                    text={"Timbre electronico"}
-                  ></Text>
-                </div>
+            <div className=" flex justify-center items-center w-3/4 mb-10 ">
+              <div className=" pl-10 w-full h-full">
+                <iframe
+                  className="rounded-md"
+                  src={extra_details?.file}
+                  width="100%"
+                  height="100%"
+                  title="PDF Viewer"
+                  frameBorder="0"
+                ></iframe>
               </div>
             </div>
           </div>
