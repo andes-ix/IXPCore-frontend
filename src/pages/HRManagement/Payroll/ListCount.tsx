56d6f77768ae105c1d98a02cba17d1fbde296124
MonasterioY <jose.monasterio@lyncros.com>
2024-09-17 16:07:05 -0400

    feat: add new detail section

--- src/pages/HRManagement/Payroll/ListCount.tsx
+++ src/pages/HRManagement/Payroll/ListCount.tsx
@@ -19,8 +19,6 @@ import {} from "services";
 import { apiPath } from "constants/env";
 import { formatDateFilter } from "Common/utils/formatDateFilter";
 import { Tooltip } from "react-tooltip";
-import { IconButton } from "yet-another-react-lightbox/*";
-// import { Tooltip } from "react-tooltip";
 
 enum fileTypeEnum {
   EXCEL = "EXCEL",
